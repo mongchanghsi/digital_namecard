65d58009b7e243392e32fc0fc7f9ec7b180f93d3
mongchanghsi <m.changhsi@gmail.com>
2021-09-19 21:10:07 +0800

    Changed qr code generation url to the url in the browser

--- src/components/QR/index.tsx
+++ src/components/QR/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import QRCode from 'qrcode';
+import QRIcon from '../../assets/qr.png';
 
 const QR = () => {
   const [showQR, setShowQR] = useState<boolean>(false);
   const [qrImage, setQRImage] = useState<string>('');
 
   const generateQR = async () => {
-    // setQRImage(await QRCode.toDataURL(window.location.href))
-    setQRImage(await QRCode.toDataURL('http://192.168.50.241:3000'));
+    setQRImage(await QRCode.toDataURL(window.location.href));
+    // setQRImage(await QRCode.toDataURL('http://192.168.50.241:3000'));
   };
 
   useEffect(() => {
@@ -24,11 +25,7 @@ const QR = () => {
         type='button'
         onClick={() => setShowQR(!showQR)}
       >
-        <img
-          className='qr_button_icon'
-          src='https://icon-library.com/images/qr-code-icon/qr-code-icon-24.jpg'
-          alt='qrIcon'
-        />
+        <img className='qr_button_icon' src={QRIcon} alt='qrIcon' />
         {showQR ? 'Hide Profile QR' : 'Generate Profile QR Code'}
       </button>
     </div>
