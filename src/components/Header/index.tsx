3be9b00aa10c9ca1f448f5a1cb429897774b0a4d
mongchanghsi <m.changhsi@gmail.com>
2021-09-27 21:17:31 +0800

    Added bio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -23,21 +23,24 @@ const Header = ({ data }: { data: any }) => {
 
   return (
     <div className='header'>
-      <div className='header_grid'>
-        <div>
-          <img
-            className='header_image'
-            src={data.profilePicture}
-            alt='profile'
-          />
-        </div>
-        <div className='header_paragraph'>
-          <p className='header_paragraph_name'>{craftName()}</p>
-          <p>{data.title}</p>
-          <p>{data.phone}</p>
-          <p>{data.email}</p>
+      <div className='header_sub'>
+        <div className='header_grid'>
+          <div>
+            <img
+              className='header_image'
+              src={data.profilePicture}
+              alt='profile'
+            />
+          </div>
+          <div className='header_paragraph'>
+            <p className='header_paragraph_name'>{craftName()}</p>
+            <p>{data.title}</p>
+            <p>{data.phone}</p>
+            <p>{data.email}</p>
+          </div>
         </div>
       </div>
+      <p className='header_bio'>{data.bio}</p>
     </div>
   );
 };
