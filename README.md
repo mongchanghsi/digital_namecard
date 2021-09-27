885fd4922a316da2d27d3cbb364d8ec00c8762a9
mongchanghsi <m.changhsi@gmail.com>
2021-09-27 21:20:02 +0800

    Updated the default data template and the readme instructions for it

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## How to use
 
 1. Clone project
-2. Using the `data_template.json` as a data template for your personal information, edit the fields accordingly
-3. Then, rename the file to `mockJson.json` and place it inside `./src/utils/mockData`
+2. Using the `data_template.ts` as a data template for your personal information, edit the fields accordingly
+3. Then, rename the file to `mockJson.ts` and place it inside `./src/utils/mockData`
 4. perform `npm install` to install the dependencies
 5. cd into the project and perform `npm start`
 6. You should be able to view the prototype in port 3000, make sure you are viewing it on mobile mode
---
+++ data_template.ts
@@ -0,0 +1,34 @@
+import GithubIcon from '../../assets/github.png';
+import PortfolioIcon from '../../assets/portfolio.png';
+import LinkedInIcon from '../../assets/linkedin.png';
+
+const data = {
+  firstName: 'YOUR FIRST NAME',
+  middleName: 'YOUR MIDDLE NAME',
+  lastName: 'YOUR LAST NAME',
+  email: 'YOUR MAIN EMAIL',
+  phone: 'YOUR MAIN CONTACT NUMBER',
+  title: 'YOUR TITLE',
+  organization: 'YOUR ORGANIZATION',
+  profilePicture: 'URL TO YOUR PROFILE PICTURE',
+  bio: 'OPTIONAL BIO FOR INTRODUCTION',
+  links: [
+    {
+      logo: 'https://icon-library.com/images/icon-github/icon-github-29.jpg',
+      text: 'Github',
+      link: 'YOUR GITHUB URL',
+    },
+    {
+      logo: 'https://icon-library.com/images/portfolio-icon/portfolio-icon-13.jpg',
+      text: 'Portfolio',
+      link: 'YOUR PORTFOLIO/WEBSITE URL',
+    },
+    {
+      logo: 'https://icon-library.com/images/linkedin-icon/linkedin-icon-8.jpg',
+      text: 'LinkedIn',
+      link: 'YOUR LINKEDIN URL',
+    },
+  ],
+};
+
+export default data;
