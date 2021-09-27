00b5b822d81879ee0cf18b98b93535f97ac7bece
mongchanghsi <m.changhsi@gmail.com>
2021-09-27 22:06:10 +0800

    Added interaction notes

--- src/components/AddContact/index.tsx
+++ src/components/AddContact/index.tsx
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import vCardGenerator from '../../utils/vCard';
 import AddIcon from '../../assets/addContact.png';
+import CloseIcon from '../../assets/close.png';
 
 const AddContact = ({ data }: { data: any }) => {
+  const [openTab, setOpenTab] = useState<boolean>(false);
+  const [text, setText] = useState<string>('');
+
   return (
-    <button
-      className='qr_button no_margin_bottom'
-      onClick={() => vCardGenerator(data)}
-    >
-      <img className='qr_button_icon' src={AddIcon} alt='contact' />
-      Add Contact
-    </button>
+    <>
+      {openTab ? (
+        <div>
+          <textarea
+            id='w3review'
+            name='w3review'
+            rows={4}
+            cols={30}
+            placeholder='[Optional] Write in a message to remember our interaction!'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+          <div className='contact_grid'>
+            <button
+              className='contact_button no_margin_bottom'
+              onClick={() => vCardGenerator(data, text)}
+            >
+              <img
+                className='contact_button_icon'
+                src={AddIcon}
+                alt='add contact'
+              />
+              Done
+            </button>
+            <button
+              className='contact_button no_margin_bottom'
+              onClick={() => setOpenTab(false)}
+            >
+              <img
+                className='contact_button_icon'
+                src={CloseIcon}
+                alt='close tab'
+              />
+              Close
+            </button>
+          </div>
+        </div>
+      ) : (
+        <button
+          className='qr_button no_margin_bottom'
+          onClick={() => setOpenTab(true)}
+        >
+          <img className='qr_button_icon' src={AddIcon} alt='contact' />
+          Add Contact
+        </button>
+      )}
+    </>
   );
 };
 
